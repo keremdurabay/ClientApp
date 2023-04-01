90820556792f6621f3840587be164b710ec5ee06
kerem <fkeremdurabay@gmail.com>
2023-04-01 23:32:28 +0300

    Update and Delete

--- src/app/components/users-table/users-table.component.ts
+++ src/app/components/users-table/users-table.component.ts
@@ -22,11 +22,27 @@ export class UsersTableComponent implements OnInit {
   users:IUser[]
   constructor(private userService: UserService) { }
   async ngOnInit(): Promise<void> {
-    this.users = await this.userService.GetAll()
-    console.log(this.users)
+    await this.GetAll()
   }
 
-  async Update(id: number): Promise<void> {
+  async GetAll(): Promise<void> {
+    this.users = await this.userService.GetAll()
+  }
+
+  async GetById(id: number): Promise<void> {
     this.UserForm.setValue(await this.userService.GetById(id))  
+  }
+
+  async Update(): Promise<void> {
+    await this.userService.Update(this.UserForm.value)
+        await this.GetAll()
+    
+  }
+
+  async Delete(id: number): Promise<void> {
+    if (confirm("Delete User?")) {
+      await this.userService.Delete(id)
+      await this.GetAll()
+    }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -16,4 +16,12 @@ export class UserService {
   async GetById(id: number): Promise<IUser> {
     return await this.http.get<IUser>(environment.ApiUrl + "/User/GetById/" + id).toPromise()
   }
+
+  async Update(data: any): Promise<number> {
+    return this.http.put<number>(environment.ApiUrl+"/User/Update",data).toPromise()
+  }
+
+  async Delete(id: number): Promise<number> {
+    return this.http.delete<number>(environment.ApiUrl + "/User/Delete/"+id).toPromise()
+  }
 }
